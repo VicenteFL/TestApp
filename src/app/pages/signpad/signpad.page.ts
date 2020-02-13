2e1e9f1659ed0eaf2323f0ce9a8ec18c6b898066
VicenteFL <vicente.flledo@alumnos.upm.es>
2020-02-13 13:52:44 +0100

    Sign is exported as a pdf  where the image is located in the bottom rigth par of the page

--- src/app/pages/signpad/signpad.page.ts
+++ src/app/pages/signpad/signpad.page.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, EventEmitter } from '@angular/core';
 import { SignaturePad } from 'angular2-signaturepad/signature-pad';
+import * as jsPDF from 'jspdf';
 
+
+var doc = new jsPDF();
 
 @Component({
   selector: 'app-signpad',
@@ -16,9 +19,9 @@ var signaturePad = new signaturePad(canvas, {
 }); */
 
 
-
 export class SignpadPage implements OnInit {
 
+  onSave = new EventEmitter();
   signature = '';
   isDrawing = false;
 
@@ -37,15 +40,31 @@ export class SignpadPage implements OnInit {
     this.Cancel = this.signatureImage; */
   }
 
-  /* drawComplete() {
-    this.signatureImage = this.signaturePad.toDataURL().replace('data:image/png;base64,', '');
-    console.log(this.signatureImage)
+  drawComplete() {
+  /* this.signatureImage = this.signaturePad.toDataURL().replace('data:image/png;base64,', '');
+  console.log(this.signatureImage) */
+    this.onSave.emit(this.signaturePad.toDataURL()); 
+    this.onSaveHandler(this.signaturePad.toDataURL());
   }
 
   drawClear() {
     this.signaturePad.clear();
+    this.onClearHandler();
   }
- */
+
+  onClearHandler() {
+    console.log('onclear clicked...');
+  }
+
+  onSaveHandler(data: any) {
+    console.log('onsave clicked');
+    console.log(data);
+    //window.open(data);
+    doc.addImage(data, 'PNG', 120, 270, 100, 20);
+    doc.output('dataurlnewwindow');
+    //window.open(doc);
+  }
+ 
   ngOnInit() {
   }
 
